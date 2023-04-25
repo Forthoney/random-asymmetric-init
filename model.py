7fc20d9cd78d00633a38e57645c584e263755d41
Forthoney <castlehoneyjung@gmail.com>
2023-04-25 00:12:57 -0400

    implement 1D model

--- model.py
+++ model.py
@@ -1,34 +1,56 @@
 import torch
 
-def make_1d_model(initializer, depth=10):
-    modules = [torch.nn.Linear(1, 2), torch.nn.ReLU()]
-    for _ in range(depth - 2):
-        modules.append(torch.nn.Linear(2, 2))
-        modules.append(torch.nn.ReLU())
+class LinearWithReLU(torch.nn.Module):
+    def __init__(self, in_features, out_features, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.linear = torch.nn.Linear(in_features, out_features)
+        self.relu = torch.nn.ReLU()
 
-    modules.extend([torch.nn.Linear(2, 1), torch.nn.ReLU(),])
+    def forward(self, x):
+        x = self.linear(x)
+        return self.relu(x)
 
-    model = torch.nn.Sequential(*modules)
-    model.apply(initializer)
 
 def init_he_normal(layer: torch.nn.Module, bias=1):
-    if not isinstance(layer, torch.nn.Linear): return
+    if not isinstance(layer, torch.nn.Linear):
+        return
 
     torch.nn.init.kaiming_normal_(layer.weight)
     layer.bias.data.fill_(bias)
 
-def init_rai(layer: torch.nn.Module):
-    if not isinstance(layer, torch.nn.Linear): return
 
-    beta_distribution = torch.distributions.Beta(torch.tensor([2]),
-                                                 torch.tensor([1]))
+def init_rai(layer: torch.nn.Module):
+    if not isinstance(layer, torch.nn.Linear):
+        return
+
+    beta_distribution = torch.distributions.Beta(
+        torch.tensor([2.0]), torch.tensor([1.0])
+    )
     fan_in, fan_out = torch.nn.init._calculate_fan_in_and_fan_out(layer.weight)
 
     V = torch.randn(fan_out, fan_in + 1) * 0.6007 / fan_in**0.5
     for i in range(fan_out):
-        j = torch.randint(0, fan_in + 1)
+        j = torch.randint(low=0, high=fan_in + 1, size=(1,))
         V[i, j] = beta_distribution.sample()
-    layer.weight = torch.transpose(V[:, :-1], 0, 1)
-    layer.bias = V[:, -1]
 
-make_1d_model(init_he_normal)
+    layer.weight = torch.nn.Parameter(torch.transpose(V[:, :-1], 0, 1))
+    layer.bias = torch.nn.Parameter(V[:, -1])
+
+
+def make_1d_model(initializer: callable, depth: int = 10, **kwargs) -> torch.nn.Module:
+    layers = torch.nn.ModuleList(
+        [
+            LinearWithReLU(1, 2),
+            *[LinearWithReLU(2, 2) for _ in range(depth - 2)],
+            LinearWithReLU(2, 1),
+        ]
+    )
+
+    model = torch.nn.Sequential(*layers)
+    model.apply(lambda layer: initializer(layer, **kwargs))
+
+    return model
+
+
+model = make_1d_model(init_he_normal, bias=0)
+print(model)
