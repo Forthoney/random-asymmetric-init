0c70b1abcb70eb5d2419a6fd61df96734e2b66dc
Forthoney <castlehoneyjung@gmail.com>
2023-05-15 20:21:48 +0900

    add automatic benchmarking functions

--- visualization.py
+++ visualization.py
@@ -6,11 +6,8 @@ import torch
 def visualize(
     f_true: Callable,
     model: torch.nn.Module,
-    x_range: Tuple[float, float],
-    resolution: int = 100,
+    x_samples: torch.Tensor,
 ):
-    x_samples = torch.linspace(x_range[0], x_range[1], resolution)
-
     plt.plot(x_samples.numpy(), f_true(x_samples.numpy()), label="True Function")
     plt.plot(
         x_samples.numpy(),
